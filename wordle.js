f37155036f96e18e39f0111a4b8ddd9601db2baf
Gelfs <91386069+Gelfs@users.noreply.github.com>
2022-05-02 23:40:33 -0500

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -228,7 +228,7 @@ function submit(guess, word) {
       highscore();
       Waudio.play();
       won();
-      setTimeout(function() {window.location.reload()}, 3000);
+      setTimeout(function() {window.location.reload();}, 3000);
      
 }
 	
