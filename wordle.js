ea8a9f8d3b3c03f71a135e8a660dee2978d7b95c
Gelfs <91386069+Gelfs@users.noreply.github.com>
2022-05-02 23:09:24 -0500

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -251,9 +251,6 @@ function won(){
 	swal("Congrats!", ", You got the correct word! Streak: " + sessionStorage.clickcount, "success");
 }
 
-document.getElementsByClassName("confirm").onclick = function() {
-	window.location.reload();
-}
 var el = document.querySelectorAll("confirm"); // this element contains more than 1 DOMs.
     for(var i =0; i < el.length; i++) {
         el[i].onclick = function() { console.log("target name should be here")};
